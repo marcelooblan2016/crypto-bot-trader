9a2e4e1615d19bdc9058f63648929f2fe4ad6dfd
Marcelo Oblan <marcelo.oblan2016@gmail.com>
2021-12-04 19:49:39 +0800

    setup metamask, load privatekey, add new network, switch network

---
+++ Metamask/Metamask.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer');
+const dappeteer = require('@chainsafe/dappeteer');
+const C = require('../constants');
+
+class Metamask {
+    constructor(options = null) {
+        this.browser = null;
+        this.page = null;
+        this.metamask = null;
+    }
+
+    async build () {
+        console.log("Launching browser...");
+        this.browser = await dappeteer.launch(puppeteer, {metamaskVersion: C.metamask_version });
+        console.log("Setup metamask...");
+        this.metamask = await dappeteer.setupMetamask(this.browser);
+        this.page = this.metamask.page;
+        // import private key
+        await this.metamask.importPK(C.private_key);
+        // add new networks
+        await this.addNewNetworks();
+        // switch to preferred network
+        console.log("Switch network: " + C.network_preferred);
+        await this.metamask.switchNetwork(C.network_preferred);
+        // import tokens
+        await this.page.waitForTimeout(999999);
+    }
+
+    async addNewNetworks () {
+        console.log("Adding new networks...");
+        let networks = C.networks;
+        let newNetworks = networks.filter( (network) => typeof network['new'] != 'undefined' && network['new'] == true);
+        for (let index in newNetworks) {
+            let network = newNetworks[index];
+            console.log("Adding network: " + network.slug);
+            await this.metamask.addNetwork({
+                networkName: network.slug,
+                rpc: network.rpc_url,
+                chainId: network.chain_id,
+                symbol: network.currency_symbol,
+                explorer: network.block_explorer_url,
+            });
+        }
+    }
+
+    selectPreferredNetwork () {
+        let networks = C.networks;
+        return networks.filter( (network) => network.slug == C.network_preferred)[0]['slug'];
+    }
+}
+
+module.exports = Metamask;
---
+++ constants.js
@@ -0,0 +1,51 @@
+const config = (() => {
+    let fs = require('fs');
+    try {
+        let envFilePath = './.env';
+        console.log("Env File: Loading...");
+
+        if (!fs.existsSync(envFilePath)) {
+            throw new Error('Env missing.')
+        }
+
+        console.log("Env File: Loaded.");
+
+        let rawData = fs.readFileSync(envFilePath, 'utf8');
+        let envValues = {};
+        let data = rawData.split("\n")
+        .filter( function (rowStr) {
+
+            return rowStr.includes('=') && !rowStr.includes('#');
+        })
+        .map( function (rawValue) {
+            let row = rawValue.split("=");
+            let value = row[1].split('"').join('');
+            envValues[row[0]] = value;
+        });
+
+        return envValues;
+    } catch(e) {
+        console.log(e);
+        process.exit(0);
+    }
+
+})();
+
+module.exports = {
+    metamask_version: 'v10.1.1',
+    private_key: config.PRIVATE_KEY,
+    network_preferred: config.PREFERRED_NETWORK,
+    networks: [
+        {"slug": "ropsten", "name": "Ropsten Test Network"},
+        {"slug": "rinkeby", "name": "Rinkeby Test Network"},
+        {
+            "slug": "matic-mainnet",
+            "name": "Matic Mainnet",
+            "new": true,
+            "rpc_url": "https://rpc-mainnet.maticvigil.com",
+            "chain_id": 137,
+            "currency_symbol": "MATIC",
+            "block_explorer_url": "https://explorer.matic.network"
+        }
+    ],
+};
