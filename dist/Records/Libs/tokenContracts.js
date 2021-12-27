2af51f715c0d424a8368c7f5e18612c779f04505
Marcelo Oblan <marcelo.oblan2016@gmail.com>
2021-12-28 07:53:43 +0800

    fixes: buymode; restructure files;

--- dist/Records/Libs/tokenContracts.js
+++ dist/Records/Libs/tokenContracts.js
@@ -1,8 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const tokenContracts_1 = __importDefault(require("../Migrations/tokenContracts"));
 function tokenContracts() {
-    let jsonContractPath = '../tokenContracts.json';
+    let fileName = 'tokenContracts.json';
+    let jsonContractPath = `./${fileName}`;
     const fs = require('fs');
+    if (!fs.existsSync(jsonContractPath)) {
+        fs.appendFileSync(jsonContractPath, JSON.stringify(tokenContracts_1.default));
+    }
     let rawData = fs.readFileSync(jsonContractPath);
     return (JSON.parse(rawData));
 }
--- src/Records/Libs/tokenContracts.ts
+++ src/Records/Libs/tokenContracts.ts
@@ -1,8 +1,16 @@
 
+import tokenContractsMigrations from '../Migrations/tokenContracts';
+
 function tokenContracts (): tokenContractInterface[] {
-    let jsonContractPath: string = '../tokenContracts.json';
+
+    let fileName: string = 'tokenContracts.json';
+    let jsonContractPath: string = `./${fileName}`;
 
     const fs = require('fs');
+    if (!fs.existsSync(jsonContractPath)) {
+        fs.appendFileSync(jsonContractPath, JSON.stringify(tokenContractsMigrations));
+    }
+
     let rawData: string = fs.readFileSync(jsonContractPath);
 
     return (JSON.parse(rawData)) as tokenContractInterface[];
