be233b5ffde9317f29e0152564e8f76247d5274f
Marcelo Oblan <marcelo.oblan2016@gmail.com>
2022-01-11 08:43:08 +0800

    focus feature implemented, token: bat, grt & sand removed

--- src/Records/Migrations/tokenContracts.js
+++ src/Records/Migrations/tokenContracts.js
@@ -56,17 +56,6 @@ export default [
     },
     {
         "network_slug": "matic-mainnet",
-        "name": "Graph Token",
-        "slug": "grt",
-        "stablecoin": false,
-        "contract": "0x5fe2b58c013d7601147dcdd68c143a77499f5531",
-        "decimals": 18,
-        "site": "https://thegraph.com/",
-        "network_url": "https://polygonscan.com/token/0x5fe2b58c013d7601147dcdd68c143a77499f5531",
-        "minimum_balance": 0
-    },
-    {
-        "network_slug": "matic-mainnet",
         "name": "Aave",
         "slug": "aave",
         "stablecoin": false,
@@ -74,17 +63,6 @@ export default [
         "decimals": 18,
         "site": "https://aave.com/",
         "network_url": "https://polygonscan.com/token/0xd6df932a45c0f255f85145f286ea0b292b21c90b",
-        "minimum_balance": 0
-    },
-    {
-        "network_slug": "matic-mainnet",
-        "name": "Basic Authentication Token (BAT)",
-        "slug": "bat",
-        "stablecoin": false,
-        "contract": "0x3cef98bb43d732e2f285ee605a8158cde967d219",
-        "decimals": 18,
-        "site": "https://basicattentiontoken.org/",
-        "network_url": "https://polygonscan.com/token/0x3cef98bb43d732e2f285ee605a8158cde967d219",
         "minimum_balance": 0
     },
     {
@@ -107,17 +85,6 @@ export default [
         "decimals": 18,
         "site": "https://sushi.com/",
         "network_url": "https://polygonscan.com/token/0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a",
-        "minimum_balance": 0
-    },
-    {
-        "network_slug": "matic-mainnet",
-        "name": "Sand",
-        "slug": "sand",
-        "stablecoin": false,
-        "contract": "0xc6d54d2f624bc83815b49d9c2203b1330b841ca0",
-        "decimals": 18,
-        "site": "http://www.sandbox.game/",
-        "network_url": "https://polygonscan.com/token/0xc6d54d2f624bc83815b49d9c2203b1330b841ca0",
         "minimum_balance": 0
     },
     {
