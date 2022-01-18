e7b7fa6507be4bf078e07cf2d33b90d70bd16055
Marcelo Oblan <marcelo.oblan2016@gmail.com>
2022-01-18 09:13:20 +0800

    remove weth

--- src/Records/Migrations/tokenContracts.js
+++ src/Records/Migrations/tokenContracts.js
@@ -119,16 +119,5 @@ export default [
         "site": "https://www.wbtc.network/",
         "network_url": "https://polygonscan.com/token/0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
         "minimum_balance": 0
-    },
-    {
-        "network_slug": "matic-mainnet",
-        "name": "Wrapped Ether",
-        "slug": "weth",
-        "stablecoin": false,
-        "contract": "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
-        "decimals": 18,
-        "site": "https://weth.io/",
-        "network_url": "https://polygonscan.com/token/0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
-        "minimum_balance": 0
     }
 ];
